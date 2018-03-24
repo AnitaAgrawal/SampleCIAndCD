ac9a1ae4a60f3d064903e40c90467488124715de
Santhosh Kumar <namp.santhosh@gmail.com>
2018-03-24 15:24:20 +0530

    added basic test case for the home vc

---
+++ SampleCIAndCDTests/HomeViewControllerTests.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewControllerTests.swift
+//  SampleCIAndCDTests
+//
+//  Created by Santhosh Nampally on 24/03/18.
+//  Copyright © 2018 Santhosh Nampally. All rights reserved.
+//
+
+import XCTest
+@testable import SampleCIAndCD
+
+class HomeViewControllerTests: XCTestCase {
+    
+    var viewController : HomeViewController?
+    override func setUp() {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        viewController = storyboard.instantiateViewController(withIdentifier: "HomeViewControllerIdentifier") as! HomeViewController
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
